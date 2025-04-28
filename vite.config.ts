73f2c69a686e005ca5429fa4cfff34c23490e1c4
Vinicius Souza <viniciuspzt@gmail.com>
2025-04-29 00:32:03 +0100

    Remove dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/viniciuspizetta.github.io/",
   plugins: [react(), tailwindcss()],
 })
