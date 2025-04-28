06732708714ef6819aed5951731d40d0fd626213
Vinicius Souza <viniciuspzt@gmail.com>
2025-04-29 00:41:50 +0100

    Update gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/viniciuspizetta.github.io/",
   plugins: [react(), tailwindcss()],
 })
