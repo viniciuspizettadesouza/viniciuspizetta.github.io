8594701784a79aad8f2dedb2a5bb9bbd10fdb8b2
Vinicius Souza <viniciuspzt@gmail.com>
2025-04-29 01:39:11 +0100

    Change favicon

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/viniciuspizetta.github.io/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
